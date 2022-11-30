9c718dc47b56242a441626fc32ac450cff4cb31c
Nanzi Mustafa Chowdhury <ge42poj@mytum.de>
2022-11-30 13:04:28 +0600

    404 page done

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,9 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Link } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp/SignUp";
 import SingleCategoryCollection from "../../Pages/SingleCategoryCollection/SingleCategoryCollection";
+import error from '../../Images/error.jpg';
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 export const router = createBrowserRouter([
@@ -29,6 +30,16 @@ export const router = createBrowserRouter([
                 element: <Login></Login>
             }
         ]
+       
+    },
+    {
+        path: '*',
+        element: <div className="text-center mt-20">
+            <h1 className="text-3xl mb-6">Page Not Found</h1>
+            <p>Looks like you have followed a broken link or entered a <span className="font-bold">URL</span> that doesn't exist on this site.</p>
+            <Link to='/' className="font-semibold text-teal-900"> Back to our site</Link>
+            <img src={error} alt="ERROR" className="max-w-5xl mx-auto"></img>            
+            </div>
     }
 ]) 
 
